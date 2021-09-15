babc551d10faf89d092cdbf9a86d7b954e06ff9b
P22inolan <p22inolan@fairfieldprep.org>
2021-09-15 10:59:16 -0400

    Lab 9/15

--- lab_1-1.py
+++ lab_1-1.py
@@ -1,4 +1,4 @@
-# Author: CAM (AMDG) 11/22/19
+# Author: IBN (AMDG) 11/22/19
 
 first_name = "Ian"
 print(first_name)
---
+++ lab_2-2.py
@@ -0,0 +1,10 @@
+# Author: IBN (AMDG) 9/15/2021
+a = 3
+b = a
+print(a)
+print(b)
+
+a = 7
+
+print(a)
+print(b)
