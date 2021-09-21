03a3881b1ef5dfa862127435cf69b085dbe37156
P22inolan <p22inolan@fairfieldprep.org>
2021-09-21 10:05:10 -0400

    Lab 6-1 9/21

---
+++ lab_6-1.py
@@ -0,0 +1,9 @@
+# Autor: IBN (AMDG) 9/21/2021
+
+first_name = input("Your first name, please. ")
+
+last_name = input("Your last name, please. ")
+
+full_name = first_name + " " + last_name
+
+print("Your full name is: " + str(full_name))
